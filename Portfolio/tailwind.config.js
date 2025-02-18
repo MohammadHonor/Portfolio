66b599bfdeaba745a9e84e8059051c6c3761c151
Sharuf <ali@gmail.com>
2025-02-19 04:17:59 +0530

    some-modification

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,23 +1,55 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    ":/index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     ":/index.html",
+//     "./src/**/*.{html,js,ts,jsx,tsx}",
 
+//   ],
+//   theme: {
+//     extend: {
+//       keyframes:{
+//         'trans-top':{
+//           '0% , 100%':{transform:'translateY(10px)'},
+//           '50%':{transform:'translateY(0px)'}
+//         }
+//       },
+//      animation:{
+//       'trans-top':'trans-top 1.5s ease-in-out infinite'
+//           },
+     
+     
+//     },
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Add paths to your files
   ],
   theme: {
     extend: {
-      keyframes:{
+
+      keyframes: {
         'trans-top':{
           '0% , 100%':{transform:'translateY(10px)'},
           '50%':{transform:'translateY(0px)'}
-        }
+        },
+        'scroll-x': {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
       },
-     animation:{
-      'trans-top':'trans-top 1.5s ease-in-out infinite'
-     } 
+      animation: {
+        'scroll-x': 'scroll-x 5s linear infinite', // Animation duration, timing, and loop
+        'trans-top':'trans-top 1.5s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
 }
-
